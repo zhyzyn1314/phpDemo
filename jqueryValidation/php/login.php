e515623e03520353ac455ee396b7aff5be4b76ee
zhy <zhyzyn@126.com>
2016-05-11 15:12:00 +0800

    zhy:mysqli jquery验证插件

---
+++ jqueryValidation/php/login.php
@@ -0,0 +1,10 @@
+<?php
+//$username = $_GET['username'];
+$username = $_POST['username'];
+$loginTime = $_POST['loginTime'];
+//if($username == 'zhy'){
+//    echo json_encode(true);
+//}else{
+//    echo json_encode(false);
+//}
+echo json_encode(true);
